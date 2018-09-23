4dc9d56b57eff8d9a272f9bb108315e2b36408b6
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-09-23 15:55:56 +0900

    [add] Greeting phase enumerate class

--- src/main/kotlin/com/lasta/api/sample/constant/GreetingPhase.kt
+++ src/main/kotlin/com/lasta/api/sample/constant/GreetingPhase.kt
@@ -1,4 +1,16 @@
 package com.lasta.api.sample.constant
 
-enum class GreetingPhase {
+enum class GreetingPhase(val greeting: String) {
+    MORNING(greeting = "Good Morning"),
+    NOON(greeting = "Good afternoon"),
+    EVENING(greeting = "Good evening");
+
+    companion object {
+        @Throws(IllegalArgumentException::class)
+        fun fromValue(text: String?): GreetingPhase {
+            text ?: throw IllegalArgumentException()
+
+            return GreetingPhase.valueOf(text.toUpperCase())
+        }
+    }
 }
