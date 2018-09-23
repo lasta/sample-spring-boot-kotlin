fa1821f63bb012f8c7102d61ca8be9f423a47ad6
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-09-23 15:56:50 +0900

    [add] GreetingService interface

--- src/main/kotlin/com/lasta/api/sample/service/GreetingService.kt
+++ src/main/kotlin/com/lasta/api/sample/service/GreetingService.kt
@@ -1,4 +1,7 @@
 package com.lasta.api.sample.service
 
+import com.lasta.api.sample.constant.GreetingPhase
+
 interface GreetingService {
+    fun greet(phase: GreetingPhase, name: String?): String
 }
