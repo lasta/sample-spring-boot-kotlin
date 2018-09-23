615c1ac8526bab2606f20e9dddf39f96bd3d945d
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-09-23 15:57:07 +0900

    [add] Implement Greeting Service

--- src/main/kotlin/com/lasta/api/sample/service/impl/GreetingServiceImpl.kt
+++ src/main/kotlin/com/lasta/api/sample/service/impl/GreetingServiceImpl.kt
@@ -1,4 +1,15 @@
 package com.lasta.api.sample.service.impl
 
-class GreetingServiceImpl {
+import com.lasta.api.sample.constant.GreetingPhase
+import com.lasta.api.sample.service.GreetingService
+import org.springframework.stereotype.Service
+
+@Service
+class GreetingServiceImpl: GreetingService {
+    override fun greet(phase: GreetingPhase, name: String?): String {
+        if (name.isNullOrBlank()) {
+            return "${phase.greeting}."
+        }
+        return "${phase.greeting}, $name."
+    }
 }
