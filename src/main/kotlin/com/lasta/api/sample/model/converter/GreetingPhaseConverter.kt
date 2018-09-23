31361f692c8071cf50d744b879ced2f21e5f4515
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-09-23 15:56:22 +0900

    [add] from String to GreetingPhase converter

--- src/main/kotlin/com/lasta/api/sample/model/converter/GreetingPhaseConverter.kt
+++ src/main/kotlin/com/lasta/api/sample/model/converter/GreetingPhaseConverter.kt
@@ -1,4 +1,11 @@
 package com.lasta.api.sample.model.converter
 
-class GreetingPhaseConverter {
+import com.lasta.api.sample.constant.GreetingPhase
+import java.beans.PropertyEditorSupport
+
+class GreetingPhaseConverter : PropertyEditorSupport() {
+    @Throws(IllegalArgumentException::class)
+    override fun setAsText(text: String?) {
+        value = GreetingPhase.fromValue(text)
+    }
 }
