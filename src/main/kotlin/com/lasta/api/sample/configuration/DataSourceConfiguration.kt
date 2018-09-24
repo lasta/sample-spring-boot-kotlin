c01994810d1d1bf5dea8c86f3faa7b2135304fe3
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-09-24 17:35:41 +0900

    [add] zip code data configuration

--- src/main/kotlin/com/lasta/api/sample/configuration/DataSourceConfiguration.kt
+++ src/main/kotlin/com/lasta/api/sample/configuration/DataSourceConfiguration.kt
@@ -1,4 +1,45 @@
 package com.lasta.api.sample.configuration
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.JpaVendorAdapter
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.orm.jpa.vendor.Database
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
+import javax.persistence.EntityManagerFactory
+import javax.sql.DataSource
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("com.lasta.api.sample.repository")
 class DataSourceConfiguration {
+
+    @Autowired
+    lateinit var dataSource: DataSource
+
+    @Bean
+    fun jpaVendorAdapter(): JpaVendorAdapter =
+            HibernateJpaVendorAdapter().apply {
+                setDatabase(Database.MYSQL)
+                setShowSql(true)
+            }
+
+    @Bean
+    fun entityManagerFactory(dataSource: DataSource): LocalContainerEntityManagerFactoryBean =
+            LocalContainerEntityManagerFactoryBean().apply {
+                setDataSource(dataSource)
+                setPackagesToScan("com.lasta.api.sample.entity")
+                this.jpaVendorAdapter = jpaVendorAdapter()
+            }
+
+    @Bean
+    fun transactionManager(entityManagerFactory: EntityManagerFactory): PlatformTransactionManager =
+            JpaTransactionManager().apply {
+                setEntityManagerFactory(entityManagerFactory)
+            }
 }
