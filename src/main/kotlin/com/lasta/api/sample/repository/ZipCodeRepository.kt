0252ceafe8e0b0b8418f5f807a64f781b5a661ab
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-09-24 17:36:00 +0900

    [add] zip code dao

--- src/main/kotlin/com/lasta/api/sample/repository/ZipCodeRepository.kt
+++ src/main/kotlin/com/lasta/api/sample/repository/ZipCodeRepository.kt
@@ -3,4 +3,6 @@ package com.lasta.api.sample.repository
 import com.lasta.api.sample.entity.ZipCodeEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ZipCodeRepository: JpaRepository<ZipCodeEntity, Long>
+interface ZipCodeRepository : JpaRepository<ZipCodeEntity, Long> {
+    fun findByZipCode(zipCode: String): List<ZipCodeEntity>
+}
