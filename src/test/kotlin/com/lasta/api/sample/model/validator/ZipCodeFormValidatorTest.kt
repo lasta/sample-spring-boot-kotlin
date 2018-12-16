7d8c92b13e03b48fd9a5b0822fa3b0492cb0c0d6
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-12-16 20:47:33 +0900

    WIP validator test

---
+++ src/test/kotlin/com/lasta/api/sample/model/validator/ZipCodeFormValidatorTest.kt
@@ -0,0 +1,45 @@
+package com.lasta.api.sample.model.validator
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Before
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations.initMocks
+import javax.validation.ConstraintValidatorContext
+
+// XXX: not works
+internal class ZipCodeFormValidatorTest {
+
+    @Mock
+    lateinit var context: ConstraintValidatorContext
+
+    @Before
+    fun setUp() {
+        initMocks(this)
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun validationTest(code: String?, expected: Boolean) {
+        val actual: Boolean = ZipCodeFormValidator().isValid(code, context)
+        assertThat(actual, equalTo(expected))
+    }
+
+    companion object {
+        @Suppress("unused")
+        @JvmStatic
+        fun cases(): List<Arguments> = listOf(
+                arguments(null, false),
+                arguments("", false),
+                arguments("123456", false),
+                arguments("1234567", true),
+                arguments("12345678", false),
+                arguments("abcdefg", false)
+        )
+    }
+}
